b8a5ced8b8bcacf2181fc8c4eefa003884e4639b
Marcin Strazynski <strazynski@gmail.com>
2014-10-20 15:32:48 +0200

    fadeOut methods

--- smartQuery-Effects.js
+++ smartQuery-Effects.js
@@ -17,4 +17,34 @@
 		this.style.display = '';
 		return this;
 	};
+
+	Element.prototype.fadeOut = function (delay) {
+		var opacity = this.style.opacity || getComputedStyle(this)['opacity'],
+			that = this,
+			timer = setInterval(function () {
+				if (opacity <= 0.1) {
+					clearInterval(timer);
+					that.style.display = 'none';
+				}
+				that.style.opacity = opacity;
+				that.style.filter = 'alpha(opacity=' + (opacity * 100) + ')';
+				opacity = parseFloat(opacity) - 0.1;
+			}, (delay || 50));
+	}
+
+	Element.prototype.fadeIn = function (delay) {
+		var opacity = this.style.opacity || getComputedStyle(this)['opacity'],
+			that = this,
+			timer = setInterval(function () {
+				if (opacity >= 1) {
+					clearInterval(timer);
+				}
+				if (that.style.display === 'none') {
+					that.style.display = 'block';
+				}				
+				that.style.opacity = opacity;
+				that.style.filter = 'alpha(opacity=' + (opacity * 100) + ')';
+				opacity = parseFloat(opacity) + 0.1;
+			}, (delay || 50));	
+	}
 })();
